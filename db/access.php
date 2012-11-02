ad030a63cb8371bccab679f0d6c9a681b4740df1
Mark Nelson <markn@moodle.com>
2012-11-02 14:32:01 +0800

    MDL-34270 blocks: adding new add block capabilities

--- db/access.php
+++ db/access.php
@@ -15,16 +15,33 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details
+ * Mnet hosts block caps.
  *
- * @package    block
- * @subpackage mnet_hosts
- * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @package    block_mnet_hosts
+ * @copyright  Mark Nelson <markn@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012091600;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012061700;        // Requires this Moodle version
-$plugin->component = 'block_mnet_hosts'; // Full name of the plugin (used for diagnostics)
+$capabilities = array(
+
+    'block/mnet_hosts:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        )
+    ),
+
+    'block/mnet_hosts:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    ),
+);
